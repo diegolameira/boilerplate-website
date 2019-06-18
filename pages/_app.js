71415ccd05d34e635442076a85a7dacc1b9d3df6
Diego Lameira <diego.lameira@dextra-sw.com>
2019-06-18 14:31:24 -0300

    feat(SEO): next SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import App, { Container } from 'next/app';
 import Router from 'next/router';
+import NextSeo, * as JsonLds from 'next-seo';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import Default from 'layouts/Default';
+import DefaultLayout from 'layouts/Default';
 import { pageview } from 'utils/analytic';
+
+import SEO from 'next-seo.config';
 
 const theme = {
   colors: {
     primary: '#0070f3',
-  }
+  },
 };
 
 Router.events.on('routeChangeComplete', url => pageview(url));
@@ -27,9 +30,13 @@ export default class extends App {
 
   render() {
     const { Component, pageProps } = this.props;
-    const Layout = Component.Layout || Default;
+    const { Layout = DefaultLayout, SEO: CompSEO } = Component;
+    const { Page: PageSEO, ...JSONLD } = CompSEO || {};
     return (
       <Container>
+        <NextSeo config={SEO} />
+        {PageSEO ? <NextSeo config={PageSEO} /> : null}
+        <JSONLDBuilder {...{ JSONLD }} />
         <ThemeProvider theme={theme}>
           <Layout>
             <Component {...pageProps} />
@@ -39,3 +46,16 @@ export default class extends App {
     );
   }
 }
+
+const JSONLDBuilder = ({ JSONLD }) => {
+  const lds = [];
+  for (const key in JSONLD) {
+    if (JSONLD.hasOwnProperty(key)) {
+      const props = JSONLD[key];
+      const Component = JsonLds[key];
+      const compiled = <Component key={key} {...props} />;
+      lds.push(compiled);
+    }
+  }
+  return <>{lds}</>;
+};
--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,51 @@ const Index = ({ quotes }) => (
 );
 
 // Index.Layout = Layout;
+// /*
+Index.SEO = {
+  Page: {
+    title: 'Page Meta Title',
+    description: 'This will be the page meta description',
+    canonical: 'https://www.canonicalurl.ie/',
+    openGraph: {
+      url: 'https://www.canonicalurl.ie/',
+      title: 'Open Graph Title',
+      description: 'Open Graph Description',
+      images: [
+        {
+          url: 'https://www.example.ie/og-image-01.jpg',
+          width: 800,
+          height: 600,
+          alt: 'Og Image Alt'
+        },
+        {
+          url: 'https://www.example.ie/og-image-02.jpg',
+          width: 900,
+          height: 800,
+          alt: 'Og Image Alt Second'
+        },
+        { url: 'https://www.example.ie/og-image-03.jpg' },
+        { url: 'https://www.example.ie/og-image-04.jpg' }
+      ]
+    }
+  },
+  ArticleJsonLd: {
+    url: "https://example.com/article",
+    title: "Article headline",
+    images: [
+      'https://example.com/photos/1x1/photo.jpg',
+      'https://example.com/photos/4x3/photo.jpg',
+      'https://example.com/photos/16x9/photo.jpg',
+    ],
+    datePublished: "2015-02-05T08:00:00+08:00",
+    dateModified: "2015-02-05T09:00:00+08:00",
+    authorName: "Jane Blogs",
+    publisherName: "Gary Meehan",
+    publisherLogo: "https://www.example.com/photos/logo.jpg",
+    description: "This is a mighty good description of this article.",
+  }
+}
+/**/
 
 Index.getInitialProps = async () => {
   const quotes = await fetchBreakingBadQuotes();
